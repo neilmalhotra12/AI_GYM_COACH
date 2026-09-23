95134750add3dc72dc01b883a5004d0822e9422e
Neil Malhotra <neilmalhotra689@gmail.com>
2026-09-23 19:20:25 +0530

    video input - webRTC

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from services.state.session_defaults import initial_session_defaults
 from services.config.workout_config import EXERCISE_OPTIONS
 from services.ui.style_loader import load_css, inject_local_font
 from services.persistence.exercise_repository import init_db
+from streamlit_webrtc import webrtc_streamer, WebRtcMode
 
 def main():
    st.set_page_config(page_title="AI Real-time GYM Coach", page_icon="🏋️", initial_sidebar_state="expanded", layout="centered")
@@ -40,7 +41,7 @@ def main():
 
          st.markdown("")
 
-         start_session_button = st.button("Start Session", width="stretch", key="start_session_button")
+         start_session_button = st.button("Start Workout", width="stretch", key="start_session_button")
 
          if start_session_button:
             st.session_state["workout_started"] = True
@@ -108,6 +109,48 @@ def main():
             st.metric("Front Knee Angle", f"{st.session_state.front_knee_angle}°")
             st.metric("Torso Angle", f"{st.session_state.torso_angle}°")
             st.metric("Balance Status", st.session_state.balance_status)
+
+   st.title("AI Real-time GYM Coach")
+   st.markdown("#### Real-time pose detection with proactive AI voice coaching")
+
+   if not workout_started:
+      st.markdown("""
+         <div style="
+            border: 10px dashed #444;
+            border-radius: 0px;
+            padding: 48px 32px;
+            text-align: center;
+            color: #888;
+            margin-top: 32px;
+         ">
+            <h2 style="color: #ccc; margin-bottom: 8px;" >👈Set your workout plan</h2>
+            <p style="font-size: 1.05rem;">
+               Choose your exercise, sets and reps in the sidebar, <br>
+               then click <strong>Start Workout</strong> to activate the camera and AI Coach
+            </p>
+         </div>
+
+      """, unsafe_allow_html= True)
+
+   else: 
+      context = webrtc_streamer(
+         key = "exercise-analysis",
+         mode = WebRtcMode.SENDRECV,
+         video_processor_factory=None,
+         rtc_configuration={"iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]}
+        ]},
+        media_stream_constraints={
+           "video": True,
+           "audio": False
+        },
+        async_processing=True
+      )
+
+   st.divider()
+
+   st.markdown("#### Workout History")
+
 
 
 
