5dd5cf7b526c4cc2370ec3ffba5ea228f139932c
Neil Malhotra <neilmalhotra689@gmail.com>
2026-09-23 09:52:01 +0530

    Sidebar UI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from services.auth.login_wall import render_login_wall
-
+from services.state.session_defaults import initial_session_defaults
+from services.config.workout_config import EXERCISE_OPTIONS
 
 def main():
    st.set_page_config(page_title="AI Real-time GYM Coach", page_icon="🏋️", initial_sidebar_state="expanded", layout="centered")
@@ -8,7 +9,99 @@ def main():
    if not render_login_wall():
       return
 
-   st.write("Hello")
+   initial_session_defaults()
+
+   workout_started = st.session_state.get("workout_started", False)
+
+   with st.sidebar:
+      st.title("AI integrated fitenss")
+
+      if st.session_state.username:
+         st.caption(f"👤Logged in as {st.session_state.username}")
+
+      st.divider()
+
+      st.subheader("Workout Plan")
+
+      if not workout_started:
+         st.selectbox("Exercise", options=EXERCISE_OPTIONS, key="plan_exercise")
+
+         st.number_input("Sets", min_value=0, max_value=15, key="plan_sets", step=1)
+
+         st.number_input("Reps", min_value=0, max_value=100, key="plan_reps", step=1)
+
+         st.markdown("")
+
+         start_session_button = st.button("Start Session", width="stretch", key="start_session_button")
+
+         if start_session_button:
+            st.session_state["workout_started"] = True
+            st.rerun()
+
+      else:
+         exercise = st.session_state.get("plan_exercise")
+         sets = st.session_state.get("plan_sets")
+         reps = st.session_state.get("plan_reps")
+
+         st.info(f"**{exercise}** -- {sets} Sets / {reps} Reps")
+
+         end_session_button = st.button("End Session", width="stretch", key="end_session_button")
+
+         if end_session_button:
+            st.session_state["workout_started"] = False
+            st.rerun()
+
+      if workout_started:
+         st.divider()
+         exercise = st.session_state.get("plan_exercise")
+         total_reps = st.session_state.get("reps")
+         current_set_reps = st.session_state.get("current_set_reps")
+         reps_per_set = st.session_state.get("plan_reps")
+         sets_completed = st.session_state.get("sets_completed")
+         target_sets = st.session_state.get("plan_sets")
+
+
+         st.subheader("Progress")
+
+         st.metric("Total Reps", f"{total_reps}")
+         st.metric("Current Set Reps", f"{current_set_reps}/{reps_per_set}")
+         st.metric("Sets Completed", f"{sets_completed}/{target_sets}")
+
+         st.divider()
+
+         if exercise == "Squats":
+            st.subheader("Squat Metrics")
+            st.metric("Knee Angle", f"{st.session_state.knee_angle}°")
+            st.metric("Back Angle", f"{st.session_state.back_angle}°")
+            st.metric("Depth Status", st.session_state.depth_status)
+
+
+         elif exercise == "Push-ups":
+            st.subheader("Push-up Metrics")
+            st.metric("Elbow Angle", f"{st.session_state.elbow_angle}°")
+            st.metric("Body Alignment", st.session_state.body_alignment)
+            st.metric("Hip Position", st.session_state.hip_status)
+
+         elif exercise == "Bicep Curls (Dumbbell)":
+            st.subheader("Curl Metrics")
+            st.metric("Elbow Angle", f"{st.session_state.elbow_angle}°")
+            st.metric("Shoulder Stability", st.session_state.shoulder_status)
+            st.metric("Swing Detection", st.session_state.swing_status)
+            
+
+         elif exercise == "Shoulder Press":
+            st.subheader("Shoulder Press Metrics")
+            st.metric("Elbow Angle", f"{st.session_state.elbow_angle}°")
+            st.metric("Arm Extension", st.session_state.extension_status)
+            st.metric("Back Arch", st.session_state.back_arch_status)
+
+         if exercise == "Lunges":
+            st.subheader("Lunge Metrics")
+            st.metric("Front Knee Angle", f"{st.session_state.front_knee_angle}°")
+            st.metric("Torso Angle", f"{st.session_state.torso_angle}°")
+            st.metric("Balance Status", st.session_state.balance_status)
+
+
 
 if __name__ == "__main__":
    main()
